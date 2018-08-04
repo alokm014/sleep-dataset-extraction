c93bf2c88ffad049bfcf21ec2e3287da1dc72ca8
ALOK MISHRA <alokm014@gmail.com>
2018-08-03 21:44:19 -0700

    Update edf_to_csv.py

--- edf_to_csv.py
+++ edf_to_csv.py
@@ -1,28 +1,28 @@
 import pyedflib
 import numpy as np
 import pandas as pd
-import xml.etree.ElementTree as ET
 import os
 
 #path of edf file
 path = 'C://Documents/mesa/polysomnography/edfs_test'
 try:
-	count=0
-	
 	for filename in os.listdir(path):
-		print filename
-		#fi=filename.split(".")[0]
-		fi="edf"
+		fi=filename.split(".")[0]
 		f_name=path+"/"+filename
-		print name
 		f = pyedflib.EdfReader(f_name)
+		
+		#getting samples from channel 8
 		sampling_freq=f.getSampleFrequency(8)
-		print sampling_freq
 		signal_data=f.readSignal(8)
+		
+		#time 
 		t=np.arange(0,len(signal_data))/sampling_freq
-		df= pd.DataFrame({'Flow': signal_data,'Epoch': t})
-		#df.head()
-		df.to_csv("F:\mesa\edf\%s%d.csv"% (fi,count), index=None)
-		count=count+1
+		
+		#dataframe of singal and time
+		df= pd.DataFrame({'Flow': signal_data,'Time': t})
+		df.head()
+		
+		# export file locaation
+		df.to_csv("F:\mesa\edf\%s.csv"% fi, index=None)
 except:
-	pass
+	print("error in reading")
